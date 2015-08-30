65b93440a2d705598358820530038660d5260177
Ryan Donnard <cheesefryplease@gmail.com>
2015-08-29 21:41:12 -0500

    Lesson form works

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,19 @@
 class LessonsController < ApplicationController
   def index
+    @lessons = Lesson.all
   end
 
   def new
     @lesson = Lesson.new
+  end
+
+  def create
+    # render text: params.inspect
+    @lesson = Lesson.new(params.require(:lesson).permit(:lesson_name, :lesson_number))
+    if @lesson.save
+      redirect_to lessons_url
+    else
+      render 'new'
+    end
   end
 end
