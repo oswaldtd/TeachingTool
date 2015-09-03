4544f63b9ab6135de42ec21c232934e9e0685047
Ryan Donnard <cheesefryplease@gmail.com>
2015-09-03 11:04:33 -0500

    Changed two migrations to better match the data types needed

--- db/migrate/20150828181614_create_lessons.rb
+++ db/migrate/20150828181614_create_lessons.rb
@@ -2,7 +2,7 @@ class CreateLessons < ActiveRecord::Migration
   def change
     create_table :lessons do |t|
       t.string :lesson_name
-      t.string :lesson_number
+      t.integer :lesson_number
 
       t.timestamps null: false
     end
--- db/migrate/20150901170430_create_books.rb
+++ db/migrate/20150901170430_create_books.rb
@@ -2,8 +2,8 @@ class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
       t.string :book_name
-      t.integer :page_start
-      t.integer :page_end
+      t.string :page_start
+      t.string :page_end
       t.integer :lesson_id
 
       t.timestamps null: false
